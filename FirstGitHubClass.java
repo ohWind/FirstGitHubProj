0bd00d73fc34736f5a56061f67a8423174909a8a
ohWind <43326510+ohWind@users.noreply.github.com>
2018-09-17 09:55:59 +0800

    优化代码
    纯英文修改（中文显示乱码）

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -7,7 +7,7 @@ public class FirstGitHubClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello!   GitHub£¡   Again£¡");
+		System.out.println("Hello!   GitHub!   Again!");
 
 	}
 
