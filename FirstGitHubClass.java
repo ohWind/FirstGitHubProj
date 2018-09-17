c54da92072189aa1628e2f4e7f83c6d523700b6f
ohWind <43326510+ohWind@users.noreply.github.com>
2018-09-17 09:23:28 +0800

    Add files via upload

---
+++ FirstGitHubClass.java
@@ -0,0 +1,14 @@
+package cn.jbit.output;
+
+public class FirstGitHubClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我的第一个GitHub程序！");
+
+	}
+
+}
